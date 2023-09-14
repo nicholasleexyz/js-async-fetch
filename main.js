14723652aef0cbb873eff8dc383b42b0d18db9cf
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-14 04:26:09 -0600

    styling

--- main.js
+++ main.js
@@ -5,20 +5,23 @@ import { setupReadButton } from "./readButton";
 
 // setupCounter(document.querySelector("#counter"));
 const url = "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users";
-
+const entries = [];
 const usersRoot = document.querySelector("#userData");
+
 async function read() {
   const data = await fetch(url).then((response) => response.json());
   console.log(data);
   // usersElem.innerHTML = JSON.stringify(data);
 
+  entries.forEach(ent => ent.remove());
+
   for (let i = 0; i < data.length; i++) {
     const element = data[i];
+
     // entry
     const entry = document.createElement("div");
     entry.classList.add('entry');
     usersRoot.appendChild(entry);
-
 
     // image
     const userImage = document.createElement("img");
@@ -30,6 +33,17 @@ async function read() {
     userName.innerHTML = `${element.user}`;
     entry.appendChild(userName);
 
+    // edit
+    const editButton = document.createElement("button");
+    editButton.innerHTML = `Edit`;
+    entry.appendChild(editButton);
+
+    // edit
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = `Delete`;
+    entry.appendChild(deleteButton);
+
+    entries.push(entry);
   }
 }
 
